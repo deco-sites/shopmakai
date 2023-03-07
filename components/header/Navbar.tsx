d88a195a6568beda08df9fcb4986c8a469f47dc6
guitavano <tavano62@gmail.com>
2023-03-07 00:01:55 -0300

    add: creating and styling mega mennu at desktop

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -3,27 +3,72 @@ import Icon from "$store/components/ui/Icon.tsx";
 import Text from "$store/components/ui/Text.tsx";
 import { Picture, Source } from "deco-sites/std/components/Picture.tsx";
 import type { ComponentChildren } from "preact";
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 
 import type { NavItem as Item } from "./NavItem.ts";
 
-function NavItem({
-  href,
-  label,
-}: { href: string; label: ComponentChildren }) {
+function NavItem(itemProps: Item) {
   return (
-    <a href={href}>
-      <Text
-        class="uppercase font-bold text-primary tracking-[1px]"
-        variant="body-regular"
+    <div class="relative hover:children:block h-full">
+      <a
+        href={itemProps.href ?? `/search?ft=${itemProps.label}`}
+        class="flex items-center font-bold px-1 h-full"
       >
-        {label}
-      </Text>
-    </a>
+        <span class="text-[14px] uppercase text-primary hover:text-pink tracking-[1px] h-full flex items-center">
+          {itemProps.label}
+        </span>
+      </a>
+      {
+        itemProps.children && <div class="bg-[#f5f5f5] w-[100vw] p-6 z-10 hidden fixed left-0">
+        <div class="max-w-[1182px] m-auto flex gap-[96px] justify-center">
+          <div class="flex flex-col gap-3">
+            {
+              itemProps.children.map(child => {
+                return (
+                  <div class="">
+                    <a href={child.href} class="uppercase text-[12px]">{child.label}</a>
+                  </div>
+                )
+              })
+            }
+          </div>
+          <div class="flex gap-[24px]">
+            {
+              itemProps.imageChildren?.map(child => {
+                return(
+                  <a href={child.href}>
+                    <Picture class="block" preload={false}>
+                      <Source
+                        fetchPriority="auto"
+                        src={child.image}
+                        width={195}
+                        height={239}
+                      />
+                      <img
+                        class="object-cover"
+                        loading="lazy"
+                        src={child.image}
+                        alt="Logo Makai"
+                      />
+                    </Picture>
+                    <p class="uppercase text-[12px] mt-[12px]">{child.label}</p>
+                  </a>
+                )
+              })
+            }
+
+          </div>
+        </div>
+      </div>
+      }
+      
+    </div>
   );
 }
 
 function Navbar({ items, logo }: {
   items: Item[];
+  logo: LiveImage;
 }) {
   return (
     <>
@@ -33,7 +78,7 @@ function Navbar({ items, logo }: {
           <HeaderButton variant="menu" />
 
           <a href="/" class="flex-grow w-[134px] flex justify-center" aria-label="Store logo">
-            <Picture class="block" preload="false">
+            <Picture class="block" preload={false}>
               <Source
                 fetchPriority="auto"
                 src={logo}
@@ -61,7 +106,7 @@ function Navbar({ items, logo }: {
       {/* Desktop Version */}
       <div class="hidden md:flex flex-row justify-between items-center h-[80px] border-b-1 border-default w-full max-w-[1300px] m-auto px-4">
         <a href="/" aria-label="Store logo" class="w-[180px]">
-          <Picture class="block" preload="false">
+          <Picture class="block" preload={false}>
             <Source
               fetchPriority="auto"
               src={logo}
@@ -76,7 +121,7 @@ function Navbar({ items, logo }: {
             />
           </Picture>
         </a>
-        <div class="flex-grow flex gap-6 justify-center">
+        <div class="flex-grow flex gap-6 justify-center items-center h-full">
           {items.map((item) => <NavItem {...item} />)}
         </div>
         <div class="flex items-center justify-end gap-6">
