b5d1d1fa1854e05d0ef61ae1ae42014650cac639
guitavano <tavano62@gmail.com>
2023-03-07 21:47:19 -0300

    add: menu hamburguer mobile

--- components/header/Menu.tsx
+++ components/header/Menu.tsx
@@ -9,16 +9,20 @@ function Menu({ items }: Props) {
     <ul class="flex-grow flex flex-col gap-2 pt-2">
       {items.map((item) => (
         <li>
-          <a class="text-black text-base" href={item.href}>{item.label}</a>
-          <ul class="ml-0.5">
-            {item.children.map((child) => (
-              <li>
-                <a class="text-gray-600 text-sm" href={child.href}>
-                  {child.label}
-                </a>
-              </li>
-            ))}
-          </ul>
+          <details class="px-5">
+            <summary class="block py-[18px] border-b-1 border-primary-extra-light">
+              <a class="uppercase font-bold text-primary" href={item.href}>{item.label}</a>
+            </summary>
+            <ul class="ml-0.5">
+              {item.children.map((child) => (
+                <li class="py-[18px] border-b-1 border-primary-extra-light">
+                  <a class="text-gray-600 text-sm" href={child.href}>
+                    {child.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </details>
         </li>
       ))}
     </ul>
