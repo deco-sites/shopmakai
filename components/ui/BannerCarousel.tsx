40405866532b71f5174ca8ddf69ee99b36c9a313
jovenan <jovenan2001@gmail.com>
2023-03-12 12:51:39 -0300

    impr: Images from home

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -2,6 +2,7 @@ import Icon from "$store/components/ui/Icon.tsx";
 import Carousel from "$store/components/ui/Carousel.tsx";
 import { Picture, Source } from "deco-sites/std/components/Picture.tsx";
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import { Head } from "$fresh/runtime.ts";
 
 export interface Image {
   /** @description desktop otimized image */
@@ -41,43 +42,62 @@ function BannerCarousel({ images, preload }: Props) {
     height: 740,
   };
 
-  return (
-    <Carousel
-      // this padding top (pt) is the aspect-ratio (height/width) value from the image below for each viewport
-      class={`w-full h-full max-w-[1843px] m-auto pt-[${aspectRatio(mobileDimension)}%] md:pt-[${aspectRatio(desktopDimension)}%] xxl:pt-[680px]`}
-      animationDuration={5}
-      dot={<Icon id="Circle" width={8} height={8} strokeWidth={2} />}
-    >
-      {images?.map(({ mobile, desktop, alt }, index) => {
-        const isFirst = index === 0;
-        const lcp = isFirst && preload;
 
-        return (
-          <Picture class="w-full h-full" preload={lcp}>
-            <Source
-              media="(max-width: 1024px)"
-              fetchPriority={lcp ? "high" : "auto"}
-              src={mobile}
-              width={768}
-              height={1024}
-            />
-            <Source
-              media="(min-width: 1025px)"
-              fetchPriority={lcp ? "high" : "auto"}
-              src={desktop}
-              width={1366}
-              height={517}
-            />
-            <img
-              class="w-full"
-              loading={lcp ? "eager" : "lazy"}
-              src={desktop}
-              alt={alt}
-            />
-          </Picture>
-        );
-      })}
-    </Carousel>
+  const allImages:string[] = []
+  images?.forEach(({mobile, desktop}) => {
+    allImages.push(mobile)
+    allImages.push(desktop)
+  })
+  
+  return (
+    <>
+      <Head>
+        {
+          allImages?.map((image) => {
+            return (
+
+              <link rel="preload" as="image" href={image}></link>
+            )
+          })
+        }
+      </Head>
+      <Carousel
+        // this padding top (pt) is the aspect-ratio (height/width) value from the image below for each viewport
+        class={`w-full h-full max-w-[1843px] m-auto pt-[${aspectRatio(mobileDimension)}%] md:pt-[${aspectRatio(desktopDimension)}%] xxl:pt-[680px]`}
+        animationDuration={5}
+        dot={<Icon id="Circle" width={8} height={8} strokeWidth={2} />}
+      >
+        {images?.map(({ mobile, desktop, alt }, index) => {
+          const isFirst = index === 0;
+          const lcp = isFirst && preload;
+
+          return (
+            <Picture class="w-full h-full" preload={lcp}>
+              <Source
+                media="(max-width: 1024px)"
+                fetchPriority={lcp ? "high" : "auto"}
+                src={mobile}
+                width={768}
+                height={1024}
+              />
+              <Source
+                media="(min-width: 1025px)"
+                fetchPriority={lcp ? "high" : "auto"}
+                src={desktop}
+                width={1366}
+                height={517}
+              />
+              <img
+                class="w-full"
+                loading={lcp ? "eager" : "lazy"}
+                src={desktop}
+                alt={alt}
+              />
+            </Picture>
+          );
+        })}
+      </Carousel>
+    </>
   );
 }
 
