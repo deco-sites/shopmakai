cdcfc6a26afc4ada9ee44185e34267122f845e15
jovenan <jovenan2001@gmail.com>
2023-03-13 19:36:41 -0300

    Fix: Image sizes

--- components/ui/Instafeed.tsx
+++ components/ui/Instafeed.tsx
@@ -58,13 +58,13 @@ function Instafeed({ medias }: Props) {
             if (typeSource == "image") {
               return (
                 <a href={href} key={index} class="w-full max-w-[180px] max-h-[180px] lg:(max-w-[300px] max-h-[300px])">
-                  <img width={300} height={300} loading="lazy" class="w-full max-w-[180px] max-h-[180px] aspectRatio object-cover lg:(max-w-[300px] max-h-[300px])" src={media} alt={alt} />
+                  <img width={150} height={150} loading="lazy" class="w-full max-w-[180px] max-h-[180px] aspectRatio object-cover lg:(max-w-[300px] max-h-[300px])" src={media} alt={alt} />
                 </a>
               )
             } else  {
               return (
                 <a href={href} key={index} class="w-full max-w-[180px] max-h-[180px] lg:(max-w-[300px] max-h-[300px])">
-                  <video width={300} height={300} preload="none" class="w-full max-w-[180px] max-h-[180px] aspectRatio object-cover lg:(max-w-[300px] max-h-[300px])" loop autoPlay muted>
+                  <video width={150} height={150} preload="none" class="w-full max-w-[180px] max-h-[180px] aspectRatio object-cover lg:(max-w-[300px] max-h-[300px])" loop autoPlay muted>
                     <source src={media} />
                   </video>
                 </a>
